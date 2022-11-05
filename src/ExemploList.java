c10269436b9903606fc55d13b0acb30709efb399
YouCanBeGod <dallasta.amos@gmail.com>
2022-11-04 22:27:01 -0300

    First commit

--- src/ExemploList.java
+++ src/ExemploList.java
@@ -7,5 +7,13 @@ public class ExemploList {
         System.out.println("Crie uma lista e adicione 7 notas: ");
 
         List<Double> notas = new ArrayList<>();
+        notas.add(7.0);
+        notas.add(8.5);
+        notas.add(9.3);
+        notas.add(5.0);
+        notas.add(7.0);
+        notas.add(0.0);
+        notas.add(3.6);
+        System.out.println(notas);
     }
 }
