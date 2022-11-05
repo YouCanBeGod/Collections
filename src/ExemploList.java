bdd5c0d6ae60e4f2026de3327857e29027d9e34a
YouCanBeGod <dallasta.amos@gmail.com>
2022-11-04 22:19:56 -0300

    First commit

--- src/ExemploList.java
+++ src/ExemploList.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExemploList {
     public static void main(String[] args) {
 
+        System.out.println("Crie uma lista e adicione 7 notas: ");
+
+        List<Double> notas = new ArrayList<>();
     }
 }
